167f03884fa8bc39ecfadced05cd3e61867ee26a
Brennan-Young <m.brennanyoung@gmail.com>
2017-04-05 16:19:33 -0700

    Spaces not tabs!

--- example.py
+++ example.py
@@ -1,11 +1,11 @@
 from api import *
 
 def main():
-	x = RiotAPI()
-	y = x.get_player_info(['liquidpiglet','doublelift','intamaterasu','everyonesma'])
-	pprint.pprint(y)
-	z = x.get_matchlist_by_name(['doublelift'])
-	pprint.pprint(z)
+    x = RiotAPI()
+    y = x.get_player_info(['liquidpiglet','doublelift','intamaterasu','everyonesma'])
+    pprint.pprint(y)
+    z = x.get_matchlist_by_name(['doublelift'])
+    pprint.pprint(z)
 
 if __name__=='__main__':
-	main()
+    main()
